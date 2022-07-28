1fe7542b31ac716cb40767c4dd5a18ea4f149bd0
kolonyavo <hei.kolo.17@gmail.com>
2022-07-28 22:38:13 +0300

    modify component

--- projet_intermediaire/src/components/uploadFile.tsx
+++ projet_intermediaire/src/components/uploadFile.tsx
@@ -1,6 +1,93 @@
 import React from "react";
 
 export default function UploadFile(){
+    
+  document.getElementById("fileToUpload").addEventListener("change", function (event) {
+    ProcessImage();
+  }, false);
+  
+  //Calls DetectFaces API and shows estimated ages of detected faces
+  function DetectFaces(imageData) {
+    AWS.region = "eu-west-2";
+    var rekognition = new AWS.Rekognition();
+    var params = {
+      Image: {
+        Bytes: imageData
+      },
+      Attributes: [
+        'ALL',
+      ]
+    };
+    rekognition.detectFaces(params, function (err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
+       var table = "<table><tr><th>Low</th><th>High</th></tr>";
+        // show each face and build out estimated age table
+        for (var i = 0; i < data.FaceDetails.length; i++) {
+          table += '<tr><td>' + data.FaceDetails[i].AgeRange.Low +
+            '</td><td>' + data.FaceDetails[i].AgeRange.High + '</td></tr>';
+        }
+        table += "</table>";
+        document.getElementById("opResult").innerHTML = table;
+      }
+    });
+  }
+  //Loads selected image and unencodes image bytes for Rekognition DetectFaces API
+  function ProcessImage() {
+    AnonLog();
+    var control = document.getElementById("fileToUpload");
+    var file = control.files[0];
+
+    // Load base64 encoded image for display 
+    var reader = new FileReader();
+    reader.onload = (function (theFile) {
+      return function (e) {
+        //Call Rekognition  
+        AWS.region = "eu-west-2";  
+        var rekognition = new AWS.Rekognition();
+        var params = {
+          Image: {
+          Bytes: e.target.result
+        },
+        Attributes: [
+        'ALL',
+      ]
+    };
+    rekognition.detectFaces(params, function (err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
+       var table = "<table><tr><th>Low</th><th>High</th></tr>";
+        // show each face and build out estimated age table
+        for (var i = 0; i < data.FaceDetails.length; i++) {
+          table += '<tr><td>' + data.FaceDetails[i].AgeRange.Low +
+            '</td><td>' + data.FaceDetails[i].AgeRange.High + '</td></tr>';
+        }
+        table += "</table>";
+        document.getElementById("opResult").innerHTML = table;
+      }
+    });
+
+      };
+    })(file);
+    reader.readAsArrayBuffer(file);
+  }
+  //Provides anonymous log on to AWS services
+  function AnonLog() {
+    
+    // Configure the credentials provider to use your identity pool
+    AWS.config.region = 'eu-west-2'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: 'eu-west-2:371cdf1c-657e-4e3f-a6a0-3cdcf905bfdc',
+    });
+    // Make the call to obtain credentials
+    AWS.config.credentials.get(function () {
+      // Credentials will be available when this function is called.
+      var accessKeyId = AWS.config.credentials.accessKeyId;
+      var secretAccessKey = AWS.config.credentials.secretAccessKey;
+      var sessionToken = AWS.config.credentials.sessionToken;
+    });
+  }
+
     return (
         <>
             <input type="file"/>
