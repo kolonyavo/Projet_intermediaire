dade28b416e67c197e4abfdceda00efc34337d7d
kolonyavo <hei.kolo.17@gmail.com>
2022-08-03 21:27:17 +0300

    update component

--- projet_intermediaire/src/App.tsx
+++ projet_intermediaire/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import UploadFile from './components/uploadFile';
+import AnalyzeImage from './components/AnalyzeImage';
 
 function App() {
   return (
     <div className="App">
-      <UploadFile/>
+      <AnalyzeImage/>
     </div>
   );
 }
